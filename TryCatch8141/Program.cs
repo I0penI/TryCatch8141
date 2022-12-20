65622e6f3bafac596e1a4c4e4ce7afdda6259ac7
ali.arslan <ali.arslan@ANK5-YZLMORT-10>
2022-12-20 13:31:42 +0300

    Add project files.

---
+++ TryCatch8141/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace TryCatch8141
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string ad = null;
+                Console.WriteLine(ad.Trim());
+            }
+            catch 
+            {
+                Console.WriteLine("Hata yapma la");
+                
+            }
+
+            try
+            {
+                string[] cocuklar = new string[3]
+                {
+                    "Ali", "Bedri", "Emir"
+                };
+                Console.WriteLine(cocuklar[3]);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine("Dosyaya loglandı: " + exc.Message);
+                
+            }
+
+            try
+            {
+                string content = File.ReadAllText(@"C:\Dosyalar\Oyunlar.txt");
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException exc)
+            {
+
+                //  throw exc;
+                Console.WriteLine("Hata Oldu");
+
+            }
+            catch(NullReferenceException exc)
+            {
+
+            }
+            catch(Exception exc)
+            {
+                
+            }
+
+            finally
+            {
+                Console.WriteLine("Try da çalışsa Catch de çalışsa finally mutlaka çalışır.");
+            }
+          
+        }
+    }
+}
